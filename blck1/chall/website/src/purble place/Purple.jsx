666a83e8fd92ecf0618a9aba2442c3b34bea9424
batuka <110884635+spaceishere@users.noreply.github.com>
2022-12-09 18:43:39 +0800

    memory hide tglom xiij duusav

--- blck1/chall/website/src/purble place/Purple.jsx
+++ blck1/chall/website/src/purble place/Purple.jsx
@@ -4,59 +4,59 @@ import Container from "@mui/material/Container";
 import { Button } from "@mui/material";
 import Purple from "./Purple.css";
 
-const Card = ({
-  dataa,
-  index,
-  setCount,
-  count,
-  setImages,
-  image,
-  ID,
-  setID,
-  setTouch,
-  touch,
-}) => {
-  const FlipCard = () => {
-    setCount(count + 1);
-    let newArr = image;
-    newArr[index].pick = !newArr[index].pick;
-    setImages([...newArr]);
-  };
-  const flipBack = () => {
-    setImages(
-      image.map((el) => {
-        if (el) {
-          el.pick = false;
-          return el;
-        } else {
-          return null;
-        }
-      })
-    );
-  };
+const GameOver = ({ totalPoint, totalMin }) => {
+  function refreshPage() {
+    window.location.reload(false);
+  }
 
-  const CheckId = (id) => {
-    if (!ID) {
-      setID(id);
-      return;
-    }
-    if (id == ID) {
-      setTimeout(() => {
-        setImages(
-          image.map((image) =>
-            image ? (image.id == id ? null : { ...image, pick: false }) : null
-          )
-        );
-        setID(null);
-        // flipBack();
-      }, 1000);
-    } else {
-      setTimeout(() => {
-        flipBack();
-      }, 400);
-      setID(null);
-    }
-  };
+  return (
+    <div
+      style={{
+        position: "absolute",
+        zIndex: "9",
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <div
+        style={{
+          width: "400px",
+          height: "300px",
+          backgroundColor: "blue",
+          justifyContent: "center",
+          alignContent: "center",
+          display: `(${totalMin == 0 ? "flex" : "none"})`,
+        }}
+      >
+        <div
+          className=""
+          style={{
+            height: "100% ",
+            display: "flex",
+            justifyContent: "space-around",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <p style={{ fontSize: "20px" }}>Good❤️😊</p>
+          <p style={{ fontSize: "20px" }}>your score:{totalPoint}</p>
+          <b>daraagin udaa ilu xicegerei amjilt ~_~</b>
+          <button
+            style={{ height: "50px", fontSize: "20px" }}
+            onClick={refreshPage}
+          >
+            play again
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const Card = ({ dataa, min, check, flipCard }) => {
   if (dataa == null) {
     return (
       <div
@@ -71,14 +71,13 @@ const Card = ({
   return (
     <div
       onClick={() => {
-        FlipCard();
-        CheckId(dataa.id);
-        setTouch(true);
+        flipCard();
       }}
       style={{
         width: "200px",
         height: "200px",
         position: "relative",
+        display: `${min == 0 ? "none" : "flex"}`,
         transform: `rotateY(${dataa.pick ? "180deg" : "0deg"})`,
         transformStyle: "preserve-3d",
         transition: "transform 0.6s",
@@ -87,11 +86,13 @@ const Card = ({
       <div
         style={{
           position: "absolute",
-          background: "red",
+          background: "transparent",
+          border: "3px solid orange",
+          borderRadius: "3%",
           width: "100%",
           height: "100%",
           backfaceVisibility: "hidden",
-          pointerEvents: `${touch ? "none " : "auto"}`,
+          pointerEvents: `${!check ? "none " : "auto"}`,
         }}
       ></div>
       <img
@@ -101,18 +102,24 @@ const Card = ({
           height: "100%",
           backfaceVisibility: "hidden",
           transform: "rotateY(180deg)",
-          pointerEvents: `${touch ? "none " : "auto"}`,
+          pointerEvents: `${!check ? "none " : "auto"}`,
         }}
       />
+      <p>{dataa.id}</p>
     </div>
   );
 };
 
 function Game() {
+  const [seconds, setSeconds] = useState(0);
+  const [min, setMin] = useState(20);
   const [ID, setID] = useState(null);
   const [count, setCount] = useState(0);
   const [images, setImages] = useState([]);
   const [touch, setTouch] = useState(false);
+  const [click, setClick] = useState(false);
+  const [score, setScore] = useState(0);
+  const [check, setCheck] = useState(false);
   const generate = () => {
     setImages(
       data.sort(function () {
@@ -120,10 +127,77 @@ function Game() {
       })
     );
   };
-  console.log(images);
+  const flip = (index) => {
+    setImages(
+      images.map((image, i) =>
+        i == index ? { ...image, pick: !image.pick } : image
+      )
+    );
+  };
+  const checkId = (index) => {
+    flip(index);
+    if (ID == null) {
+      console.log("first");
+      setID(index);
+      return;
+    }
+    console.log("second");
+
+    if (index !== ID && images[index].id === images[ID].id) {
+      setTimeout(() => {
+        setImages(
+          images.map((image, ind) =>
+            image
+              ? ind == index || ind === ID
+                ? null
+                : { ...image, pick: false }
+              : null
+          )
+        );
+        setID(null);
+        // flipBack();
+      }, 1000);
+      setScore((e) => e + 1);
+    } else {
+      setTimeout(() => {
+        flipBack();
+      }, 400);
+      setID(null);
+    }
+  };
+
+  const flipBack = () => {
+    setImages(
+      images.map((el) => {
+        if (el) {
+          el.pick = false;
+          return el;
+        } else {
+          return null;
+        }
+      })
+    );
+  };
+
   useEffect(() => {
     generate();
   }, []);
+  useEffect(() => {
+    if (click) {
+      const interval = setInterval(() => {
+        setSeconds(seconds + 1);
+      }, 10);
+      if (seconds >= 100) {
+        setMin(min - 1);
+        setSeconds(0);
+      }
+      if (min == 0) {
+        setMin(0);
+      }
+
+      return () => clearInterval(interval);
+    }
+  }, [seconds, click]);
   return (
     <body
       style={{
@@ -133,16 +207,53 @@ function Game() {
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
+        flexDirection: "column",
+        gap: "30px",
+        backgroundImage: `url("https://wallpaper.dog/large/10720023.jpg")`,
       }}
     >
+      {(min === 0 || score == 6) && (
+        <GameOver totalPoint={score} totalMin={min} />
+      )}
       <Container
         sx={{
-          display: "flex",
+          display: `${min === 0 ? "none" : "flex"}`,
+
           flexDirection: "column",
           gap: "50px",
           alignItems: "center",
         }}
       >
+        <div
+          style={{
+            width: "100%",
+            display: `${min === 0 || score == 6 ? "none" : "flex"}`,
+            justifyContent: "center",
+            gap: "150px",
+            position: "absolute",
+            zIndex: "1",
+            marginTop: "-130px",
+          }}
+        >
+          <p
+            style={{
+              fontSize: "35px",
+              fontStyle: "italic",
+              color: "orange",
+            }}
+          >
+            Time:{min}
+          </p>
+          <p
+            style={{
+              fontSize: "35px",
+              fontStyle: "italic",
+              color: "orange",
+            }}
+          >
+            Score:{score}
+          </p>
+        </div>
         <div
           className=""
           style={{
@@ -156,22 +267,33 @@ function Game() {
           {images?.map((dataa, index) => {
             return (
               <Card
-                dataa={dataa}
-                index={index}
-                setCount={setCount}
-                count={count}
-                setImages={setImages}
-                image={images}
-                setID={setID}
-                ID={ID}
                 key={index}
-                setTouch={setTouch}
-                touch={touch}
+                dataa={dataa}
+                min={min}
+                check={check}
+                flipCard={() => {
+                  checkId(index);
+                }}
               />
             );
           })}
         </div>
       </Container>
+      <Button
+        sx={{
+          display: `${min === 0 || score == 6 ? "none" : "flex"}`,
+          backgroundColor: "warning ",
+          border: "1px solid gray",
+          width: "120px",
+        }}
+        onClick={() => {
+          setClick(true);
+          setCheck(true);
+          console.log(check);
+        }}
+      >
+        Start
+      </Button>
     </body>
   );
 }
